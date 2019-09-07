58ed0bfa7e41721c662ec5de975ea8de753e5a6a
gonzalojs <eogutier@gmail.com>
2019-09-07 01:18:26 -0400

    user reg and log

--- vuexprus/routes/api/users.js
+++ vuexprus/routes/api/users.js
@@ -5,6 +5,7 @@ const router = Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
+const key = require('../../config/keys').secret
 const User = require('../../models/User')
 
 
@@ -72,5 +73,64 @@ router.post('/register', (req, res) => {
 })
 
 
+/**
+ * @route POST api/users/login
+ * @desc Signing the User
+ * @access Public
+ */
+
+router.post('/login', (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }).then(user => {
+    if (!user) {
+      return res.status(404).json({
+        msg: 'Username was not found',
+        success: false
+      })
+    }
+    //if there is user, we compare the password
+    bcrypt.compare(req.body.password, user.password).then(isMatch => {
+      if (isMatch) {
+        //User's password is correct and we need to send the JSON token for that user
+        const payload = {
+          _id: user._id,
+          username: user.username,
+          name: user.name,
+          email: user.email
+        }
+        jwt.sign(payload, key, {
+          expiresIn: 604800
+        }, (err, token) => {
+          res.status(200).json({
+            success: true,
+            token: `Bearer ${token}`,
+            msg: 'You are now logged in'
+          })
+        })
+
+      } else {
+        return res.status(404).json({
+          msg: 'Incorrect Password',
+          success: false
+        })
+      }
+    })
+  })
+})
+
+/**
+ * @route POST api/users/profile
+ * @desc Return the user's data
+ * @access Private
+ */
+
+router.get('/profile', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  return res.json({
+    user: req.user
+  })
+})
 
 module.exports = router
