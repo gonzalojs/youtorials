350c7a1784572a49882a4f019a788cc1da1a7933
gonzalojs <eogutier@gmail.com>
2019-09-11 22:49:40 -0300

    arcsecond

--- parser/index.js
+++ parser/index.js
@@ -1,2 +1,11 @@
 const A = require('arcsecond')
 
+
+const stringParser =A.str('hello').map(result => ({
+  type: 'captured string',
+  value: result
+}))
+
+console.log(
+  stringParser.run('hello')
+)
