e05aeccda5fe7908e92f7e206074641526a29edc
gonzalojs <eogutier@gmail.com>
2019-09-23 21:19:40 -0300

    user model

---
+++ mongoosential/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+module.exports = mongoose.model('User', Schema({
+  _id: Schema.Types.ObjectId,
+  name: {
+    types: String,
+    required: true,
+    validate: {
+      validate: function (text) {
+        return text.length > 0
+      },
+      message: "Empty name is not allowed"
+    }
+  }
+}))
